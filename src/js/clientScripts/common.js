d33ba7484c98ab60219fcd658d7c4e24bc077057
Jörg Grabitzky <jgrabitzky@gmail.com>
2023-12-15 10:00:22 +0100

    common.js preisanzeige angepasst

--- src/js/clientScripts/common.js
+++ src/js/clientScripts/common.js
@@ -45,7 +45,7 @@ function updateCookie(param, qty = null, cookieID = null) {
 
 function updateOPrice(price) {
   for (let el of document.getElementsByClassName('otherPrice')) {
-    el.innerText = 'Ár: ' + price + ' Ft';
+    el.innerText = 'Preis: ' + price + ' €';
   }
 }
 
