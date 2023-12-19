164a86cf7498740ab17155cc70a35cf0c400ec41
Joerg Grabitzky <joer@grabitzky.info>
2023-12-19 09:59:49 +0100

    .

--- src/js/itemLogic.js
+++ src/js/itemLogic.js
@@ -98,7 +98,7 @@ const buildItemSection = (conn, itemId, req) => {
             <div class="itemInfo">
               <p class="prodName hideText">${productName}</p>
               <p class="itemPrice">
-                <span id="priceHolder">${price}</span> Ft
+                <span id="priceHolder">${price}</span> €
               </p>
               <p class="gotham">
                 <span id="sizeHolder">${size}</span>
